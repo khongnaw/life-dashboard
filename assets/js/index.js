72f7e6c70f6386daf2ae5fa8121cac72b84b9945
khongnaw <android.prok@gmail.com>
2016-10-23 18:46:25 -1000

    play with chart.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,4 +4,42 @@ $(document).ready(function(){
 	//('.carousel').carousel();
      $('.modal-trigger').leanModal();
 });
+var config = {
+     type: 'doughnut',
+     data: {
+          datasets: [{
+               data: [20,65]
+               ,
+               backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)'
+               ],
+               label: 'Dataset 1'
+          }],
+          labels: [
+               "Age",
+               "Year Left"
+          ]
+     },
+     options: {
+          responsive: true,
+          legend: {
+               position: 'top',
+          },
+          title: {
+               display: true
+              //text: 'Chart.js Doughnut Chart'
+          },
+          animation: {
+               animateScale: true,
+               animateRotate: true
+          }
+     }
+};
 
+window.onload = function() {
+
+     var ctx = document.getElementById("chartMy1").getContext("2d");
+     window.myDoughnut = new Chart(ctx, config);
+
+};
