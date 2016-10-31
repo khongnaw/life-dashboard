992eb7eaf69cf6a291fc7d3407572c9dbc8dc178
khongnaw <android.prok@gmail.com>
2016-10-31 00:08:01 -1000

    play with jquery

--- assets/js/dashboard-script.js
+++ assets/js/dashboard-script.js
@@ -1,6 +1,78 @@
+
+var eStates = {
+     Adding: 1,
+     Substracting: 2,
+     Idle: 3,
+}
+var fCurrentState = eStates.Idle;
+var fCurrentThis = null;
+var fMousePressed = false;
 $(document).ready(function(){
+
+     setInterval(stateMachine, 100);
+
      $('.modal-trigger').leanModal();
+
+     $('.btn-add-pt').mousedown(function() {
+          fCurrentState = eStates.Adding;
+          fCurrentThis = this;
+          fMousePressed = true;
+     })
+     $('.btn-add-pt').mouseup(function() {
+          fMousePressed = false;
+     })
+     $('.btn-minus-pt').mousedown(function() {
+          fCurrentState = eStates.Substracting;
+          fCurrentThis = this;
+          fMousePressed = true;
+     })
+     $('.btn-minus-pt').mouseup(function() {
+          fMousePressed = false;
+     })
+
 });
+var stateMachine = function () {
+     switch (fCurrentState){
+          case eStates.Adding:
+               if(fMousePressed) {
+                    add_progress(fCurrentThis);
+               }else {
+                    fCurrentState = eStates.Idle;
+                    fCurrentThis = null;
+               }
+               break;
+          case eStates.Substracting:
+              if(fMousePressed) {
+                   subtrct_progress(fCurrentThis);
+              }else {
+                   fCurrentState = eStates.Idle;
+                   fCurrentThis = null;
+              }
+               break;
+          case  eStates.Idle:
+             break;
+     }
+}
+var add_progress = function (this_) {
+     var num =  parseInt($(this_).siblings('.progress-num').find('span.num').text());
+     var den =  parseInt($(this_).siblings('.progress-num').find('span.den').text());
+     num = (num + 1 <= den) ? num + 1: num;
+     update_progress(this_,num,den)
+}
+var subtrct_progress = function (this_) {
+     var num =  parseInt($(this_).siblings('.progress-num').find('span.num').text());
+     var den =  parseInt($(this_).siblings('.progress-num').find('span.den').text());
+     num = (num - 1 >= 0) ? num - 1: num;
+     update_progress(this_,num,den)
+}
+var update_progress = function (this_, num, den) {
+     $(this_).siblings('.progress-num').find('span.num').text(num)
+     $(this_).siblings('.progress-num').find('span.den').text(den)
+
+     var percent = (num / den) * 100 + '%';
+     $(this_).siblings('.progress').children('.determinate').css('width',percent);
+}
+
 var config = {
      type: 'doughnut',
      data: {
@@ -35,7 +107,6 @@ var config = {
           }
      }
 };
-
 window.onload = function() {
 
      // var ctx = document.getElementById("chartMy1").getContext("2d");
